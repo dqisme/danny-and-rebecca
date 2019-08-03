6fcac62815f00c7899a5a00c99e4f9b054a64c7a
dqisme <dqisme@outlook.com>
2019-08-03 12:50:34 +0800

    [dq] add index page.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'docs'),
     filename: 'bundle.js'
-  }
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: "Rebecca & Danny",
+  })],
 };
