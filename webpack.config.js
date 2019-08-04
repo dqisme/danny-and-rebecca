48d9100c09acf66e1edd55236cf84b5290586d6b
dqisme <dqisme@outlook.com>
2019-08-04 12:01:48 +0800

    [dq] add style and dev server.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,25 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const outputPath = path.resolve(__dirname, 'docs');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'docs'),
+    path: outputPath,
     filename: 'bundle.js'
   },
   plugins: [new HtmlWebpackPlugin({
     title: "Rebecca & Danny",
   })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: outputPath,
+  }
 };
