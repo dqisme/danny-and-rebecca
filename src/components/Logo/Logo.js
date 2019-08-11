f486216e71d1a9810d61a1ba25b6670a70cb5d63
dqisme <dqisme@outlook.com>
2019-08-11 13:40:21 +0800

    [dq] add animation to logo.

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,39 @@
 import { createElement } from "../../utils";
-import LogoSvg from './Logo.svg';
+import anime from "animejs/lib/anime.es";
+import DHtml from './D.svg';
+import AndHtml from './And.svg';
+import RHtml from './R.svg';
 import './Logo.css';
 
-const Logo = createElement(LogoSvg, 'logo');
+const Logo = createElement('div', 'logo');
+Logo.append(createElement(DHtml, 'D'));
+Logo.append(createElement(AndHtml, 'And'));
+Logo.append(createElement(RHtml, 'R'));
+
+let animationD, animationR, animationAnd;
+Logo.addEventListener('mouseenter', () => {
+  animationD = anime({
+    targets: '#D',
+    translateX: 100,
+    rotate: 10,
+    easing: 'spring',
+  });
+  animationR = anime({
+    targets: '#R',
+    translateX: -100,
+    rotate: -10,
+    easing: 'spring',
+  });
+  animationAnd = anime({
+    targets: '#And',
+    opacity: 0.5,
+    scale: 0.5,
+  });
+});
+Logo.addEventListener('mouseleave', () => {
+  animationD.reset();
+  animationR.reset();
+  animationAnd.reset();
+});
 
 export default Logo;
